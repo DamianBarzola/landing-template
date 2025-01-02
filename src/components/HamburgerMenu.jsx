852589db5dbd972808018096766b951c6326f523
DamianBarzola <dbdamix@gmail.com>
2025-01-02 09:44:25 -0300

    Add initial project setup with Tailwind CSS, React integration, and basic layout components

---
+++ src/components/HamburgerMenu.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import menuIcon from "../assets/icons/menu.svg";
+import closeIcon from "../assets/icons/close.svg";
+const NAV_STYLES = {
+  backgroundColor: "#0e0e0e",
+  color: "whitesmoke",
+};
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  return (
+    <>
+      {isOpen ? (
+        <img
+          className="hover:cursor-pointer w-10"
+          src={closeIcon.src}
+          onClick={toggleMenu}
+          alt="Menú"
+        />
+      ) : (
+        <img
+          className="hover:cursor-pointer w-10"
+          src={menuIcon.src}
+          onClick={toggleMenu}
+          alt="Menú"
+        />
+      )}
+      <nav
+        className={`fixed mt-20 z-10 top-0 right-0 w-full h-full transform transition-all duration-300 ease-in-out 
+          ${
+            isOpen
+              ? "-translate-x-0 opacity-100 scale-100"
+              : "translate-x-full opacity-0 scale-95"
+          }`}
+        style={NAV_STYLES}
+        id="responsive-menu"
+      >
+        <div id="menu-items" className="flex flex-col items-center mt-4 gap-2">
+          <div
+            className="flex justify-center w-full py-4 hover:opacity-85 hover:cursor-pointer"
+            id="menu-item"
+          >
+            <span className="text-xl">asdasd</span>
+          </div>
+          <div
+            className="flex justify-center w-full py-4 hover:opacity-85 hover:cursor-pointer"
+            id="menu-item"
+          >
+            <span className="text-xl">asdasd</span>
+          </div>
+          <div
+            className="flex justify-center w-full py-4 hover:opacity-85 hover:cursor-pointer"
+            id="menu-item"
+          >
+            <span className="text-xl">asdasd</span>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default HamburgerMenu;
